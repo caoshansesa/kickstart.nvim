9f9b5510af21d4298851df3e4346c007a670899a
Shan CAO <113851256+caoshansesa@users.noreply.github.com>
2024-07-11 13:35:45 -0400

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,48 @@
--- vim-fugitive
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+local function my_on_attach(bufnr)
+  local api = require 'nvim-tree.api'
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts 'Up')
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+end
+
+-- pass to setup along with your other options
+require('nvim-tree').setup {
+  ---
+  on_attach = my_on_attach,
+  ---
+}
+
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = { on_attach = my_on_attach },
+  },
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    build = ':Copilot auth',
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  -- vim-fugitive
   {
     'tpope/vim-fugitive',
     keys = {
@@ -7,22 +51,4 @@
       { '<leader>gc', ':Commits<CR>', desc = 'Git commits' },
     },
   },
-  -- Neo tree
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-      '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    opts = {
-      window = {
-        fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
-          ['<C-j>'] = 'move_cursor_down',
-          ['<C-k>'] = 'move_cursor_up',
-        },
-      },
-    },
-  },
+}
